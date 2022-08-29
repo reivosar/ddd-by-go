20b82c4faa1695cec6664d45407b30aa5c786316
reivosar <users.noreply.github.com>
2022-08-29 16:41:46 +0900

    env file added

--- application/usecase/user/query/userserchcriteria.go
+++ application/usecase/user/query/userserchcriteria.go
@@ -1,6 +1,6 @@
 package query
 
 type UserSearchCriteria struct {
-	UserId   string `json:"userId" binding:"required"`
-	UserName string `json:"userName" binding:"required"`
+	UserId   string `json:"userId"`
+	UserName string `json:"userName"`
 }
--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"ddd-by-go/Interface/api/user"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	loadEnv()
 	router := gin.Default()
 	user.SetupRouter(router)
-	router.Run("8081")
+	router.Run(":" + os.Getenv("PORT_IN_ADDRESS"))
+}
+
+func loadEnv() {
+	if err := godotenv.Load(".env"); err != nil {
+		panic(err)
+	}
 }
